40dd216e9514839f63a0aef93780befb23c5508d
JaredLMayer <JaredLMayer@gmail.com>
2019-08-04 14:38:03 -0400

    have workable data for prices and ticker symbol.

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-require('dotenv').config({ silent: process.env.NODE_ENV === 'production' });
+// require('dotenv').config({ silent: process.env.NODE_ENV === 'production' });
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
